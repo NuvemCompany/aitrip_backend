67a2f091cae8d6cddac93d6b98312e9227e462e2
Gabriel Souza <contato@devgabrielsouza.com.br>
2024-06-13 23:23:04 -0300

    feat(main): add global error interceptors and dynamic port configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NotFoundInterceptor } from './common/errors/interceptors/notfound.interceptor';
+import { UnauthorizedInterceptor } from './common/errors/interceptors/unauthorized.interceptor';
+import { ConflictInterceptor } from './common/errors/interceptors/conflict.interceptor';
+import { DatabaseInterceptor } from './common/errors/interceptors/database.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalInterceptors(new NotFoundInterceptor());
+  app.useGlobalInterceptors(new UnauthorizedInterceptor());
+  app.useGlobalInterceptors(new ConflictInterceptor());
+  app.useGlobalInterceptors(new DatabaseInterceptor());
+  await app.listen(process.env.APP_PORT || 3000);
 }
 bootstrap();
