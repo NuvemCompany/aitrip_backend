cec46cc16265328ac2d0256c037f98af8d90c45e
Gabriel Souza <contato@devgabrielsouza.com.br>
2024-09-15 14:23:52 -0300

    chore(commitlint): update commitlint configuration
    - Modified commitlint.config.js to allow emojis and improve commit message validation

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,9 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-empty': [2, 'never'],
+    'subject-empty': [2, 'never'],
+    'type-case': [0],
+    'subject-case': [0],
+  },
+};
